d9f263d6a28e0e24a30ebeb7ac88f97404f50a2c
rifqmd <rifqim110@gmail.com>
2025-09-18 00:39:51 +0700

    feat: graph paid invoice

---
+++ app/components/InvoiceGraph.tsx
@@ -0,0 +1,81 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Graph from "./Graph";
+import prisma from "../utils/db";
+import { requireUser } from "../utils/hooks";
+
+async function getInvoiceData(userId: string) {
+  const rawData = await prisma.invoice.findMany({
+    where: {
+      userId: userId,
+      status: "PAID",
+      createdAt: {
+        lte: new Date(),
+        gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // last 30 days
+      },
+    },
+    select: {
+      createdAt: true,
+      total: true,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  // group and aggregate data by date
+  const aggregatedData = rawData.reduce(
+    (acc: { [key: string]: number }, curr) => {
+      const dateKey = new Date(curr.createdAt).toLocaleDateString("id-ID", {
+        month: "short",
+        day: "numeric",
+      });
+
+      acc[dateKey] = (acc[dateKey] || 0) + curr.total;
+
+      return acc;
+    },
+    {} // <-- initial value for accumulator
+  );
+
+  // Transform data to match the graph's expected format
+  const transformData = Object.entries(aggregatedData)
+    .map(([date, amount]) => ({
+      date,
+      amount,
+      originalDate: new Date(date + "," + new Date().getFullYear()),
+    }))
+    .sort((a, b) => a.originalDate.getTime() - b.originalDate.getTime())
+    .map(({ date, amount }) => ({
+      date,
+      amount,
+    }));
+
+  return transformData;
+}
+
+export default async function InvoiceGraph() {
+  const session = await requireUser();
+  const data = await getInvoiceData(session.user?.id as string);
+
+  console.log(data);
+
+  return (
+    <Card className="lg:col-span-2">
+      <CardHeader>
+        <CardTitle>Paid Invoices</CardTitle>
+        <CardDescription>
+          Invoices which have been paid in the last 30 days
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Graph data={data} />
+      </CardContent>
+    </Card>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import DashboardBlocks from "../components/DashboardBlocks";
+import InvoiceGraph from "../components/InvoiceGraph";
 import { signOut } from "../utils/auth";
 import { requireUser } from "../utils/hooks";
 
@@ -8,6 +9,13 @@ export default async function DashboardRoute() {
   return (
     <>
       <DashboardBlocks />
+      <div className="grid grid-4 lg:grid-cols-3 md:gap-8">
+        {/* col-span-2 70% */}
+        <InvoiceGraph />
+
+        <h1 className="bg-red-500 col-span-1">this is about 30%</h1>
+      </div>
+
       <form
         action={async () => {
           "use server";
