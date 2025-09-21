71c4ce4a2dc04297d0c143f00fea8994eafe2cd2
rifqmd <rifqim110@gmail.com>
2025-09-21 23:15:23 +0700

    fix: delete rules esline branch dev

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,8 +22,6 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       // "@typescript-eslint/no-unused-vars": "off",
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
     },
   },
 ];
