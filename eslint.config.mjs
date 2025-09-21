dfab52d6033dfe0f8d5524e5e568a676b5652196
rifqmd <rifqim110@gmail.com>
2025-09-21 23:14:13 +0700

    fix: add rules for build ignore esline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,9 +21,8 @@ const eslintConfig = [
     ],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      // "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
     },
   },
 ];
